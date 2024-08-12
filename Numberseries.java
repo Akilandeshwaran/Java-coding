47864b01ab5aa2e9ff521f5ed6812fface92e889
Akilandeshwaran <146162216+Akilandeshwaran@users.noreply.github.com>
2024-08-12 16:00:28 +0530

    Create Numberseries.java

---
+++ Numberseries.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class jana{
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        int b = a;int i=0;int j=0;
+        int c=a;int m=0;
+        for(int k = 0;k<a;k++)
+        {
+        for(i=0;i<a;i++){
+            for(j=0;j<c;j++){
+                System.out.print(b);
+            }
+            b=b-1;
+        }
+        System.out.println();
+        m=m+1;
+        b=a;
+        c=a-m;
+        }
+    }
+    }
